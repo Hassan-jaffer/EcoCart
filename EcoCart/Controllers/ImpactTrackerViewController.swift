734b4d9f2c77c21250506a5a52078fe7f2df0940
Hasoooni2003 <162882041+Hasoooni2003@users.noreply.github.com>
2024-12-01 23:09:21 +0300

    fixed build error, added DGCharts package

---
+++ EcoCart/Controllers/ImpactTrackerViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ImpactTrackerViewController.swift
+//  EcoCart
+//
+//  Created by Hasan Shehab on 01/12/2024.
+//
+
+import UIKit
+import DGCharts
+class ImpactTrackerViewController: UIViewController {
+
+    @IBOutlet weak var chartView: HorizontalBarChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setData()
+    }
+    
+    func setData() {
+        let labels = ["CO2 Saved", "Plastic Waste Reduced", "Impact on Trees", "Biodegradability"]
+        let values: [Double] = [100, 200, 300, 400]
+        var entries: [BarChartDataEntry] = []
+        for (i, value) in values.enumerated() {
+            let entry = BarChartDataEntry(x: value, y: Double(i))
+            entries.append(entry)
+        }
+        let dataSet = BarChartDataSet(entries: entries,label: "Impact Metrics")
+        dataSet.colors = [NSUIColor.systemGreen]
+        dataSet.valueColors = [NSUIColor.black]
+        
+        let data = BarChartData(dataSet: dataSet)
+        chartView.data = data
+        
+        chartView.leftAxis.valueFormatter = IndexAxisValueFormatter(values: labels)
+        chartView.rightAxis.enabled = false
+        chartView.xAxis.enabled = true
+        chartView.xAxis.labelPosition = .bottom
+        
+        }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
