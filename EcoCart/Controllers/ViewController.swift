afcd330a3928759068ab2b8ed6220e3c1e9fa813
mmahmood233 <m7mdm233@gmail.com>
2024-12-05 04:09:29 +0300

    product details

--- EcoCart/Controllers/ViewController.swift
+++ EcoCart/Controllers/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var testButton: UIButton!
+    
+    // MARK: - Properties
+    let testProductId = "E3a7t5anTprKCgJdrnpX"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
+    
+    private func setupUI() {
+        title = "EcoCart"
+        
+        testButton.backgroundColor = .systemGreen
+        testButton.setTitle("Test Product Details", for: .normal)
+        testButton.setTitleColor(.white, for: .normal)
+        testButton.layer.cornerRadius = 10
+        testButton.titleLabel?.font = .systemFont(ofSize: 17, weight: .semibold)
+        
+        testButton.layer.shadowColor = UIColor.black.cgColor
+        testButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        testButton.layer.shadowRadius = 4
+        testButton.layer.shadowOpacity = 0.1
+    }
+    
+    @IBAction func testButtonTapped(_ sender: Any) {
+        if let productDetailsVC = ProductDetailsViewController.instantiate(with: testProductId) {
+            let backButton = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+            navigationItem.backBarButtonItem = backButton
+            
+            navigationController?.pushViewController(productDetailsVC, animated: true)
+        }
+    }
 }
-
