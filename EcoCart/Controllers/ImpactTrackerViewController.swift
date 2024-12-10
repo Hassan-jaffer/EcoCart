9b9aba95998e6900a30a0dc9e557824050cf27c5
Hasoooni2003 <162882041+Hasoooni2003@users.noreply.github.com>
2024-12-10 22:17:50 +0300

    improved functionality

--- EcoCart/Controllers/ImpactTrackerViewController.swift
+++ EcoCart/Controllers/ImpactTrackerViewController.swift
@@ -33,35 +33,22 @@ class ImpactTrackerViewController: UIViewController {
     }
     
     func editProgressView(){
-        //co2 bar
-        co2progressView.transform = co2progressView.transform.scaledBy(x: 1, y: 5)
-        co2progressView.layer.cornerRadius = 10
-        co2progressView.clipsToBounds = true
-        co2progressView.layer.borderColor = UIColor.black.cgColor
-        co2progressView.layer.borderWidth = 0.1
+        editProgress(co2progressView)
+        editProgress(plasticProgressView)
+        editProgress(impOnTreeProgressView)
+        editProgress(bioProgressView)
         
-        //plastic bar
-        plasticProgressView.transform = plasticProgressView.transform.scaledBy(x: 1, y: 5)
-        plasticProgressView.layer.cornerRadius = 10
-        plasticProgressView.clipsToBounds = true
-        plasticProgressView.layer.borderColor = UIColor.black.cgColor
-        plasticProgressView.layer.borderWidth = 0.1
-        
-        //tree bar
-        impOnTreeProgressView.transform = impOnTreeProgressView.transform.scaledBy(x: 1, y: 5)
-        impOnTreeProgressView.layer.cornerRadius = 10
-        impOnTreeProgressView.clipsToBounds = true
-        impOnTreeProgressView.layer.borderColor = UIColor.black.cgColor
-        impOnTreeProgressView.layer.borderWidth = 0.1
-        
-        //bio bar
-        bioProgressView.transform = bioProgressView.transform.scaledBy(x: 1, y: 5)
-        bioProgressView.layer.cornerRadius = 10
-        bioProgressView.clipsToBounds = true
-        bioProgressView.layer.borderColor = UIColor.black.cgColor
-        bioProgressView.layer.borderWidth = 0.1
     }
-    
+    func editProgress(_ progress: UIProgressView){
+        
+        progress.transform = progress.transform.scaledBy(x: 1, y: 5)
+        progress.layer.cornerRadius = 10
+        progress.clipsToBounds = true
+        progress.layer.borderColor = UIColor.black.cgColor
+        progress.layer.borderWidth = 0.1
+        
+        
+    }
     func updateProgressView(){
         co2progressView.setProgress(0.8, animated: true)
         co2Percentage.text = "80%"
@@ -71,6 +58,7 @@ class ImpactTrackerViewController: UIViewController {
         treePercentage.text = "40%"
         bioProgressView.setProgress(0.2, animated: true)
         bioPercentage.text = "20%"
+        //TODO - get user's data and update the progress views (above is demo only)
     }
     
     func editGraphView(){
@@ -79,43 +67,38 @@ class ImpactTrackerViewController: UIViewController {
         graphView.layer.borderWidth = 2
         graphView.layer.borderColor = UIColor.systemGreen.cgColor
         graphView.layer.shadowColor = UIColor.black.cgColor
-
         
         //reset button
         resetBtn.layer.cornerRadius = 15
         
     }
-    /*func setData() {
-        let labels = ["CO2 Saved", "Plastic Waste Reduced", "Impact on Trees", "Biodegradability"]
-        let values: [Double] = [100, 200, 300, 400]
-        var entries: [BarChartDataEntry] = []
-        for (i, value) in values.enumerated() {
-            let entry = BarChartDataEntry(x: Double(i), y: value)
-            entries.append(entry)
-        }
-        let dataSet = BarChartDataSet(entries: entries,label: "Impact Metrics")
-        dataSet.valueFormatter = DefaultValueFormatter(decimals: 0)
-        dataSet.colors = [NSUIColor.systemGreen]
-        dataSet.valueColors = [NSUIColor.black]
-        
-        let data = BarChartData(dataSet: dataSet)
-        chartView.data = data
-        
-        chartView.leftAxis.valueFormatter = IndexAxisValueFormatter(values: labels)
-        chartView.rightAxis.enabled = false
-        chartView.xAxis.enabled = true
-        chartView.xAxis.labelPosition = .bottom
-        
-        }
-     */
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func resetBtnTapped(_ sender: Any) {
+        showAlert()
     }
-    */
+    
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "Reset data?", message: "Are you sure you want to reset all data? This action cannot be undone", preferredStyle: .alert)
+        
+        let actionReset = UIAlertAction(title: "Confirm", style: .destructive){ action in
+            self.resetData() // call reset data method
+        }
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(actionCancel)
+        alert.addAction(actionReset)
+        
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
+    
+    func resetData(){
+        //TODO - clear all user's data
+        
+    }
+    
+    
+    
 
 }
